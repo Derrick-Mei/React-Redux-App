6872d9f9ed3524683711da2fdbc13745dfa460b8
Derrick Mei <derrick.mei.168@gmail.com>
2019-10-27 07:36:12 -0500

    completed SearchForm

--- async-redux/src/components/SearchForm.js
+++ async-redux/src/components/SearchForm.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
 
-export default function Search() {
-    return (
-        <form>
-            <label>
-                <input type="text" placeholder="Title"/>
-            </label>
-        </form>
-    )
+import { handleChange, handleSubmit } from "../actions";
+
+function Search(props) {
+  return (
+    <form onSubmit={() => props.handleSubmit(props.searchString)}>
+      <label>
+        Search your favorite Anime!
+        <input
+          type="text"
+          placeholder={props.searchString}
+          value={props.searchString}
+          onChange={e => props.handleChange(e.target.value)}
+        />
+      </label>
+      <button type="submit" disabled={props.start}>
+        Submit
+      </button>
+    </form>
+  );
 }
+
+const mapStateToProps = state => {
+  return {
+    searchString: state.formReducer.searchString,
+    start: state.animeReducer.start,
+    success: state.animeReducer.success,
+    error: state.animeReducer.error
+  };
+};
+
+const mapDispatchToProps = { handleChange, handleSubmit };
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
