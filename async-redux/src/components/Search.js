8374caa916ace3c0a5aea6b68e9b2b8efb3a840b
Derrick Mei <derrick.mei.168@gmail.com>
2019-10-27 05:27:31 -0500

    set up file structure

---
+++ async-redux/src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Search() {
+    return (
+        <form>
+            <label>
+                <input type="text" placeholder="Title"/>
+            </label>
+        </form>
+    )
+}
---
+++ async-redux/src/reducers/formReducer.js
@@ -0,0 +1,2 @@
+// Optimally a form component should have state locally
+// I'm implementing it using the reducer to demonstrate the combineReducer
