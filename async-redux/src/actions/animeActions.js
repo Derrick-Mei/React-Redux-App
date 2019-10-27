62467153e14729b504cb4f77d235a4212c1b3630
Derrick Mei <derrick.mei.168@gmail.com>
2019-10-27 07:19:35 -0500

    setup redux store
    wrapped index with provider
    created root reducer
    applied thunk and logger middleware

--- async-redux/src/actions/animeActions.js
+++ async-redux/src/actions/animeActions.js
@@ -1,10 +1,10 @@
 export const HANDLE_SUBMIT = "HANDLE_SUBMIT";
-export const AXIOS_START = "AXIOS_START";
-export const AXIOS_SUCCESS = "AXIOS_SUCCESS";
-export const AXIOS_ERROR = "AXIOS_START";
+export const HANDLE_START = "HANDLE_START";
+export const HANDLE_SUCCESS = "HANDLE_SUCCESS";
+export const HANDLE_ERROR = "HANDLE_START";
 
 export function handleSubmit(input) {
   return dispatch => {
-    dispatch({ type: AXIOS_START });
+    dispatch({ type: HANDLE_START });
   };
 }
--- async-redux/src/actions/index.js
+++ async-redux/src/actions/index.js
@@ -1,8 +1,8 @@
-export { HANDLE_CHANGE, handleSubmit } from "./formActions";
+export { HANDLE_CHANGE, handleChange } from "./formActions";
 export {
   HANDLE_SUBMIT,
   HANDLE_START,
   HANDLE_SUCCESS,
   HANDLE_ERROR,
-  handleChange
+  handleSubmit
 } from "./animeActions";
