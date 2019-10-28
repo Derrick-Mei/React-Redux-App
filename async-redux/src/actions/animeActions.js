2ff6606292ccdc7d61748fabf536ce5f54eb4244
Derrick Mei <derrick.mei.168@gmail.com>
2019-10-28 04:20:08 -0500

    completed anime actions and reducers for async actions

--- async-redux/src/actions/animeActions.js
+++ async-redux/src/actions/animeActions.js
@@ -1,10 +1,23 @@
+import fetchAnime from "../utils/fetch";
+
 export const HANDLE_SUBMIT = "HANDLE_SUBMIT";
 export const HANDLE_START = "HANDLE_START";
 export const HANDLE_SUCCESS = "HANDLE_SUCCESS";
 export const HANDLE_ERROR = "HANDLE_START";
 
-export function handleSubmit(input) {
+export function handleSubmit(searchString) {
   return dispatch => {
     dispatch({ type: HANDLE_START });
+
+    fetchAnime(searchString)
+      .then(res => res.json().then(json => (res.ok ? json : Promise.reject(json))))
+      .then(data => {
+        dispatch({ type: HANDLE_SUCCESS, payload: data.data });
+        console.log("Data: ", data);
+      })
+      .catch(err => {
+        dispatch({ type: HANDLE_ERROR });
+        console.log("Error: ", err);
+      });
   };
 }
--- async-redux/src/reducers/animeReducer.js
+++ async-redux/src/reducers/animeReducer.js
@@ -1,15 +1,36 @@
-import { HANDLE_SUBMIT } from "../actions";
+import { HANDLE_START, HANDLE_SUCCESS, HANDLE_ERROR } from "../actions";
 
 export const initialState = {
-  start: false,
-  success: false,
-  error: false
+  isLoading: false,
+  error: false,
+  data: {
+    Page: {
+      pageInfo:{},
+      media: []
+    }
+  }
 };
 
 export function animeReducer(state = initialState, action) {
   switch (action.type) {
-    case HANDLE_SUBMIT:
-      return state;
+    case HANDLE_START:
+      return {
+        isLoading: true,
+        error: false,
+        data: []
+      };
+    case HANDLE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload
+      };
+    case HANDLE_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: true
+      };
     default:
       return state;
   }
